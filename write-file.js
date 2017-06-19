0a8d56af54659c7ccccb89da5ba9908d8f086c96
Priit Pärl <priitparl@gmail.com>
2017-06-20 00:14:28 +0300

    first

---
+++ write-file.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const getCurrentTimeAndDate = () => {
+    // Time
+    let hours = (new Date()).getHours();
+    hours = (hours < 10 ? '0' : '') + hours;
+    let minutes = (new Date()).getMinutes();
+    minutes = (minutes < 10 ? '0' : '') + minutes;
+    let seconds = (new Date()).getSeconds();
+    seconds = (seconds < 10 ? '0' : '') + seconds;
+
+    // Date
+    let year = (new Date()).getFullYear();
+    let month = (new Date()).getMonth() + 1;
+    month = (month < 10 ? '0' : '') + month;
+    let day = (new Date()).getDate();
+    day = (day < 10 ? '0' : '') + day;
+
+    return `Time: ${hours}:${minutes}:${seconds} | Date: ${day}.${month}.${year} |`;
+
+};
+
+const myMessagePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Tere, Priit!');
+    }, 200);
+
+    setTimeout(() => {
+        reject(`Could not handle message!`);
+    }, 1);
+});
+
+myMessagePromise
+    .then(data => console.log(data))
+    .catch(error => {
+        errorHandler(error);
+    });
+
+const errorHandler = error => {
+    const errorMesssage = `${getCurrentTimeAndDate()} Error: ${error}`;
+
+    fs.appendFile('text.txt', `${errorMesssage}\n`, 'utf8', err => {
+        if (err) throw err;
+    });
+};
