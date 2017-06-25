d9f2987d6ac9948c2212546e7cee9aa3d60f6039
Priit Pärl <priitparl@gmail.com>
2017-06-25 21:24:41 +0300

    filesize

---
+++ fs/fileSize.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const fileSize = (filename, cb) => {
+    if (typeof filename !== 'string') {
+        return cb(new TypeError('Argument should be a string!'));
+    }
+
+    fs.stat(filename, (err, stats) => {
+        if (err) return cb(err);
+
+        cb(null, stats.size);
+    });
+}
+
+fileSize(__filename, (err, size) => {
+    if (err) throw err;
+
+    console.log(`Size in KB: ${size/1024}`);
+});
+
+console.log('Hello');
